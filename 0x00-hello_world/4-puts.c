e73aceb27a7cce566b05569d5f8eaa6ee2caf649
leehomeecky <meecky4christ@gmail.com>
2023-03-16 09:47:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-        char output[] = "\"Programming is like building a multilingual puzzle";
-        puts(output);
+	char output[] = "\"Programming is like building a multilingual puzzle";
 
-        return 0;
+	puts(output);
+
+	return (0);
 }
