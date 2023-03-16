2e290baef2b6e21dc264d43f8cdc76a474348f92
leehomeecky <meecky4christ@gmail.com>
2023-03-16 09:34:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 int main (void)
 {
-        char out[] = "\"Programming is like building a multilingual puzzle\n";
-        puts(out);
+        char output[] = "\"Programming is like building a multilingual puzzle";
+        puts(output);
 
         return 0;
 }
