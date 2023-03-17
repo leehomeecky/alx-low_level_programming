708a66ab92d58cd1599585afb6b1e154a8a85c10
leehomeecky <meecky4christ@gmail.com>
2023-03-17 10:43:36 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,13 +17,13 @@ for (i = 0; i <= 9; i++)
 {
 	for (j = 0; j <= 9; j++)
 	{
-		for (k = 0; k <= 9; k++)
+		for (k = i; k <= 9; k++)
 		{
 			for (l = 0; l <= 9; l++)
 			{
 				if(k == 0 && l == 0)
 				{
-					l+=1;
+					l= j + 1;
 				};
 
 				putchar(i + '0');
