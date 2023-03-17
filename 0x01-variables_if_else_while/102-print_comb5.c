d6d51da758e475be322cd20da06edc151ec53acf
leehomeecky <meecky4christ@gmail.com>
2023-03-17 10:15:28 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				if(k == 0 && l == 0)
+				{
+					l+=1;
+				};
+
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+				putchar(k + '0');
+				putchar(l + '0');
+
+				if(i != 9 || j != 8 || k != 9 || l != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			};
+		};
+	};
+};
+
+return (0);
+}
