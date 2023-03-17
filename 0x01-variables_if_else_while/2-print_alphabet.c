902449ed03f5ed7c1d0b062c8a0eda133741207e
leehomeecky <meecky4christ@gmail.com>
2023-03-17 07:31:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Code to output a-z to the console
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	};
+
+	putchar('n\');
+
+	return (0);
+}
