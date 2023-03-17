c575af6d7d76785be782a8692a036c2153c0b78e
leehomeecky <meecky4christ@gmail.com>
2023-03-17 08:10:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char _;
+
+	for (_ = '0'; i <= '9'; i++)
+	{
+		putchar(_);
+	};
+
+	putchar('\n');
+
+	return (0);
+}
