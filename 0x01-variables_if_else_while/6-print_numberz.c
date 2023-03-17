2938617fb57492a191508369fef47022ac96aed4
leehomeecky <meecky4christ@gmail.com>
2023-03-17 08:13:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char _;
+
+	for (_ = '0'; _ <= '9'; _++)
+	{
+		putchar(_);
+	};
+
+	putchar('\n');
+
+	return (0);
+}
