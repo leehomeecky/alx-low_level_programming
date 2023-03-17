7344d5ef5dad3ecd0230bf775cd0dcf38824e1f1
leehomeecky <meecky4christ@gmail.com>
2023-03-17 08:46:03 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Code to output a-zA-Z to the console
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'o'; i <= '9'; i++)
+	{
+		putchar(i);
+	};
+
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	};
+
+	putchar('\n');
+
+	return (0);
+}
