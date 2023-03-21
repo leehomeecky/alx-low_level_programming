fb560708538ac180525247c206a1d9ce61f795ea
leehomeecky <meecky4christ@gmail.com>
2023-03-21 09:01:50 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if the int value represent a lower case in Ascii
+ *
+ * Return: 1 if character is lower, else return 0;
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	
+	return (0);
+}
