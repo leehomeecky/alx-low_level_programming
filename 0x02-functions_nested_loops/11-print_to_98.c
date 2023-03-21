eb0f5ee8021cc844121b7bf4c5ff829471234cb3
leehomeecky <meecky4christ@gmail.com>
2023-03-21 15:21:41 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,8 @@
 
 void print_to_98(int n)
 {
+if (n <= 98)
+{
 	for (; n <= 98; n++)
 	{
 		if (n != 98)
@@ -21,6 +23,21 @@ void print_to_98(int n)
 			printf("%d", n);
 		}
 	}
-
+}
+else
+{
+	for (; n >= 98; n--)
+	{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+}
+	
 	printf("\n");
 }
