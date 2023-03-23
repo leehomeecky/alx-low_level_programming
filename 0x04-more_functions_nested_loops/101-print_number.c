74b8b9bf202c73d4d80c08c8687454294ccee411
leehomeecky <meecky4christ@gmail.com>
2023-03-23 13:23:45 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,19 @@
 
 void print_number(int n)
 {
-	unsigned int n_value, i;
-	int digit, j;
-
-	n_value = i = n;
-	digit = 1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-	n *= (-1);
-	n_value = i = n;
-	_putchar('-');
+		n *= -1;
+		k = n;
+		_putchar('-');
 	}
 
-	while ((i / 10) > 0)
-	{
-	digit++;
-	i /= 10;
-	}
+	k /= 10;
 
-	while (digit > 0)
-	{
-		i = n;
-		j = 1;
+	if (k != 0)
+		print_number(k);
 
-		while (j <= digit)
-		{
-		if (j == digit)
-			_putchar((i % 10) + '0');
-		i /= 10;
-		j++;
-		}
-
-		digit--;
-	}
+	_putchar((unsigned int) n % 10 + '0');
 }
