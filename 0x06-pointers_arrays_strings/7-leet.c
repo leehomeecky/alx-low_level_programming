2dfb3facf7a5f6bb3bb19090f5e3811bd61001ef
leehomeecky <meecky4christ@gmail.com>
2023-03-29 15:58:04 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,20 +11,24 @@
 char *leet(char *str)
 {
 	char *initial = str;
+	char *values = "AaEeOoTtLl";
+	char digit[] = "4433007711";
+	int i = 0;
+	char *start = values;
+
 
 	while (*str)
 	{
-		if (*str == 'a' || *str == 'A')
-		*str = '4';
-		else if (*str == 'e' || *str == 'E')
-		*str = '3';
-		else if (*str == 'o' || *str == 'O')
-		*str = '0';
-		else if (*str == 't' || *str == 'T')
-		*str = '7';
-		else if (*str == 'l' || *str == 'L')
-		*str = '1';
+		while (*values)
+		{
+		if (*str == *values)
+		*str = digit[i];
 
+		values++;
+		i++;
+		}
+
+		values = start;
 		str++;
 	}
 
