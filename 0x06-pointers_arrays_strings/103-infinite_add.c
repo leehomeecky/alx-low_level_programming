6aa993bc7b6d583e4fc9f1404e7b59857ba53715
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-03-30 18:45:45 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,41 +13,59 @@
   
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int carry = 0;
-    int n1_len = strlen(n1);
-    int n2_len = strlen(n2);
-    int i, j;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-    // Check if result will fit in buffer
-    if (n1_len + n2_len + 1 > size_r)
-        return 0;
-
-    // Initialize result string to '\0'
-    for (i = 0; i < size_r; i++)
-        r[i] = '\0';
-
-    // Add digits from right to left
-    for (i = n1_len - 1, j = n2_len - 1; i >= 0 || j >= 0; i--, j--)
-    {
-        int sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-        carry = sum / 10;
-        r[i + j + 1] = sum % 10 + '0';
-    }
-
-    // Add carry digit, if any
-    if (carry)
-    {
-        if (n1_len + n2_len >= size_r)
-            return 0;
-        for (i = n1_len + n2_len - 1; i >= 0; i--)
-            r[i + 1] = r[i];
-        r[0] = carry + '0';
-    }
-
-    return r;
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
+	{
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
+	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
+	return (r);
 }
 
