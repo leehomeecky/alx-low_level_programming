03dcfc9defba00b19236b952ef13c4cf788c27c2
leehomeecky <meecky4christ@gmail.com>
2023-03-30 10:09:00 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * infinite_add - a function that adds two numbers.
+ *
+ * @n1: number 1 to be added
+ * @n2: number 2 to be added
+ * @r: the buffer that the function will use to store the result
+ * @size_r: is the buffer size
+ *
+ * Return: r if Success
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
