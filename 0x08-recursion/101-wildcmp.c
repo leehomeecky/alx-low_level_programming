df671db968f88b305d53945b0d5f77a64cee1a7c
leehomeecky <meecky4christ@gmail.com>
2023-04-05 12:57:14 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -35,7 +35,9 @@ char *check(char *s1, char *s2)
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 != '\0' || *s2 != '\0')
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else
 	{
 		if (*s2 == '*')
 		{
@@ -53,5 +55,5 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 		return (wildcmp(++s1, ++s2));
 	}
-	return (1);
+	return (0);
 }
