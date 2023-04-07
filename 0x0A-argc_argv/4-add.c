36605397b20267f0f129270fd28fac0d75f5242a
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-04-07 19:35:35 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,24 +9,25 @@
  * @argc: amount of arguments, pased to the function
  * @argv: argument wariable
  *
- * Return: 0 if success
+ * Return: 0 if success else 1
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, j = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-	if (!isdigit(*argv[i]))
-	{
-	printf("%s\n", "Error");
-	return (1);
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
 	}
-
-	j += atoi(argv[i]);
-	}
-	printf("%i\n", j);
-
+	printf("%i\n", sum);
 	return (0);
 }
