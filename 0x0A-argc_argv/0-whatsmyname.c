64642b4bf7c25a77066c6d9c54ebec225272c341
leehomeecky <meecky4christ@gmail.com>
2023-04-07 09:59:22 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,8 +10,9 @@
  * Return: 0 if success
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	printf("%s\n", *argv);
 	return (0);
 }
