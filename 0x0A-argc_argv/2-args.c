e3662decfb053cf23663ee5a78802bed3b1afd49
leehomeecky <meecky4christ@gmail.com>
2023-04-07 10:15:02 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: amount of arguments, pased to the function
+ * @argv: argument wariable
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
