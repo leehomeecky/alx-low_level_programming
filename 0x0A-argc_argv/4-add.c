15d1a694f9edf375019a6ca21ac25f90d13d4e78
leehomeecky <meecky4christ@gmail.com>
2023-04-07 10:57:18 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: amount of arguments, pased to the function
+ * @argv: argument wariable
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j = 0;
+
+	if (argc < 2)
+	{
+	printf("%i\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+		if (!isdigit(argv[i]))
+		{
+		printf("%s\n", "Error");
+		return (1);
+		}
+
+		j += atoi(argv[i]);
+		}
+		printf("%i\n", j);
+	}
+	return (0);
+}
