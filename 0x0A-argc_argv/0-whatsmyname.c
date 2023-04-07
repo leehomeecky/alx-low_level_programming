ae1da4a90aba3fb30fe4e6c08487faa339d69033
leehomeecky <meecky4christ@gmail.com>
2023-04-07 09:45:46 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: amount of arguments, pased to the function
+ * @argv: argument wariable
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s", argv[0]);
+}
