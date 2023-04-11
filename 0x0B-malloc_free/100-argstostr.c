236d74c0c63606ed0a8e8666fc4e8324eeaef7aa
leehomeecky <meecky4christ@gmail.com>
2023-04-11 08:22:08 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include "main.h"
+
+int str_len(char *str);
+/**
+ * str_len - a function to get the legnth of string
+ *
+ * @str: pointer to string to be counted
+ *
+ * Return: string length
+ */
+
+
+int str_len(char *str)
+{
+	if (*str == '\0')
+	return (0);
+	return (1 + str_len(++str));
+}
+
+
+/**
+ * argstostr - a function that concatenates all the arguments of your program.
+ *
+ * @ac: number of argument.
+ * @av: array of arguments.
+ *
+ * Return: pointer to concatinated string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *space;
+	int i, j, k = 0, size = 0;
+
+	if (ac > 1)
+	{
+	for (i = 1; i < ac; i++)
+	size += str_len(av[i]);
+	space = (char *)malloc((size + ac - 2) * sizeof(char));
+	if (space != NULL)
+	{
+		for (i = 1; i < ac; i++)
+		{
+			for (j = 0; j < str_len(av[i]); j++)
+			{
+			space[k] = av[i][j];
+			k++;
+			}
+			if (i < ac - 1)
+			{
+			space[k] = '\n';
+			k++;
+			}
+		}
+		return (space);
+	}
+	}
+	return ('\0');
+}
