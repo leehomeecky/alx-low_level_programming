cd17284d1735ba385ff4313fa80134172fbe62f3
leehomeecky <meecky4christ@gmail.com>
2023-04-11 07:08:48 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,7 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-		for (i = 0; j < height; i++)
-		free(grid[i]);
-		free(grid);
+	for (i = 0; i < height; i++)
+	free(grid[i]);
+	free(grid);
 }
