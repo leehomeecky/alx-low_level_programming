4b377585c4bac05f487e0a53bcd0389a9d99492f
leehomeecky <meecky4christ@gmail.com>
2023-04-11 17:00:26 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -62,7 +62,7 @@ char **strtow(char *str)
 	int i, j, height, width;
 	char **space;
 
-	if (str == NULL || *str == ' ')
+	if (str == NULL || *str == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
 	height = word_count(str);
 	space = (char **)malloc((height + 1) * sizeof(char *));
