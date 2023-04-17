228e07ae0866dcfb00227a0875a5f93925682ddb
leehomeecky <meecky4christ@gmail.com>
2023-04-17 10:26:04 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs
+ *
+ * @d: ponter to dog structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
