953500e72cdfbe379afce6823e4a652d22941452
leehomeecky <meecky4christ@gmail.com>
2023-04-17 07:15:51 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog structure type
+ *
+ * @name: name of the dog;
+ * @owner: owner of the dog;
+ * @age: age of the dog
+ */
+
+struct dog
+{
+char *name;
+char *owner;
+float age;
+}
+
+#endif
