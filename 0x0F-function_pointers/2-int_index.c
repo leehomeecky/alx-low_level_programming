a001da7d3140eb18adf474b821a46d337f85a831
leehomeecky <meecky4christ@gmail.com>
2023-04-19 05:40:25 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size > 0 && cmp != NULL)
+	if (size > 0 && array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]) != 0)
