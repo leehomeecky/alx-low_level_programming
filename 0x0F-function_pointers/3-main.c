b1404e94cfbfe6dd391895f33c16372cfce79a5b
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-04-19 21:40:23 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,26 +11,35 @@
  * Return: 0 if success
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*i)(int, int);
+	int num1, num2;
+	char *op;
 
-	if (argc > 3)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2] == "%" || argv[2] == "/") && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	i = get_op_func(argv[2]);
-	if (i == NULL)
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", i(atoi(argv[1]), atoi(argv[3])));
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
