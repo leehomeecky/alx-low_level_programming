264d7081121886f5efc6557616115ea18646d56b
leehomeecky <meecky4christ@gmail.com>
2023-04-19 13:32:02 -0700

    3-main.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,11 +2,11 @@
 #define CALC_H
 
 /**
-   * struct op - Struct op
-    *
-     * @op: The operator
-      * @f: The function associated
-       */
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
 char *op;
