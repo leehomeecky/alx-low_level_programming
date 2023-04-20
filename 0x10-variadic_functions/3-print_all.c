52547646e91f97b358d0537147d5325ad9147ebe
leehomeecky <meecky4christ@gmail.com>
2023-04-20 13:59:32 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_str(va_list args);
+void print_flt(va_list args);
+
+/**
+ * print_char - prints a character
+ *
+ * @args: valued argument;
+ */
+
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints integer
+ *
+ * @args: valued argument;
+ */
+
+void print_int(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * print_str - prints strings
+ *
+ * @args: valued argument;
+ */
+
+void print_str(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+
+	if (s == NULL || *s == '\0')
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", va_arg(args, char *));
+}
+
+/**
+ * print_flt - prints float
+ *
+ * @args: valued argument;
+ */
+
+void print_flt(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_all - a function that prints anything.
+ *
+ * @format: a list of types of arguments passed to the function
+ * @...: are variable aguement
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j;
+	char *q = "";
+	print_f x[] = {
+			{"i", print_int},
+			{"c", print_char},
+			{"s", print_str},
+			{"f", print_flt}
+		};
+
+	va_start(args, format);
+	while (format && *(format + i))
+	{
+		j = 0;
+		while (j < 4 && *(format + i) != *((x + j)->idn))
+			j++;
+		if (j < 4)
+		{
+		printf("%s", q);
+		(x + j)->prt_func(args);
+		q = ",";
+		}
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
+/**
+ * struct print_stc - structure for print functions, and identifier;
+ *
+ * @idn: identifier for function;
+ * @prt_func: print function pointer;
+ */
+
+typedef struct print_stc
+	{
+	char *idn;
+	void (*prt_func)(va_list args);
+	} print_f;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
