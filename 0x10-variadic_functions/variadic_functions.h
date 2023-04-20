463534925f8b0d943efb28b0c126415e5caa5d96
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-04-20 23:12:01 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,22 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
 
 /**
- * struct print_stc - structure for print functions, and identifier;
+ * struct printer - A new struct type defining a printer.
  *
- * @idn: identifier for function;
- * @prt_func: print function pointer;
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
  */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
 
-typedef struct print_stc
-	{
-	char *idn;
-	void (*prt_func)(va_list args);
-	} print_f;
+} printer_t;
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
