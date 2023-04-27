fb4c6c76c868a102a57b7d2029f5a2fe1311b611
leehomeecky <meecky4christ@gmail.com>
2023-04-27 08:27:46 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * construct - a constructor function, it is executed before the mai function
+ */
+
+void construct(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
