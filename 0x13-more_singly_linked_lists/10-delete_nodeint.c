63dbd6512b988296cc50005b671119c160621805
leehomeecky <meecky4christ@gmail.com>
2023-04-30 06:06:37 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 		}
 		temp2 = temp->next;
-		temp = temp2->next;
+		temp->next = temp2->next;
 		free(temp2);
 	}
 	return (1);
