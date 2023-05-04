129e5899b4009ce897f5e1d12826b2b2179456d2
leehomeecky <meecky4christ@gmail.com>
2023-05-04 10:06:39 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,11 +13,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	for (i = 0; i < index; i++)
 	{
-	n = n >> 1;
 	if (n == 0)
 		return (-1);
+	n = n >> 1;
 	}
 	return (n & 1);
 }
