51d4ec042c062572969ec064c708464e2352aef6
leehomeecky <meecky4christ@gmail.com>
2023-05-04 11:27:51 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (sizeof(unsigned long int) * 8 <= index)
 	return (-1);
