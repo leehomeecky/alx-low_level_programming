e093a2fe7cf0c0ae8aeaea72d9f3685d530aee0e
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-05-04 15:45:22 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * str_len - a function to grt the length of a string
+ *
+ * @str: pointer to string to be counted
+ *
+ * Return: length of string
+ */
+
+int str_len(const char *str)
+{
+	if (*str == '\0')
+		return (0);
+
+	return (1 + str_len(++str));
+}
+
+/**
  * binary_to_uint - function that converts a binary number to an unsigned int.
  *
  * @b: pointing to a string of 0 and 1 chars
@@ -10,23 +26,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
+	unsigned int num = 0, mul = 1;
 	int len;
 
 	if (*b == '\0' || b == NULL)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	len = str_len(b);
+	len--;
+	while (len >= 0)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[len] != '1' && b[len] != '0')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		num += (b[len] - '0') * mul;
+		mul *= 2;
+		len--;
 	}
-
 	return (num);
 }
