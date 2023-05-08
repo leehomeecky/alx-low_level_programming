5f74e41fd565656e70e7d4ed6860d448c50af4eb
leehomeecky <meecky4christ@gmail.com>
2023-05-08 13:49:33 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main = entry point
+ *
+ * @argc: number of argument passed in
+ * @argv: arguments pased in
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
