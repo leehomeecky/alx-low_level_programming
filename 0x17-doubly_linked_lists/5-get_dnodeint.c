163f082b837cce8d528cdae1cf6ef1e4df6cafd3
leehomeecky <meecky4christ@gmail.com>
2023-06-16 00:36:35 +0100

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,13 +12,12 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int i;
-
-	for (i = 0; i < index; i++)
+	for (; index != 0; index--)
 	{
 		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
+
 	return (head);
 }
